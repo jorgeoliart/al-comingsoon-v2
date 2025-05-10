17897f06b243105e9692ac24ad6a3eb5999ed298
Jorge Oliart <jorgeoliart@users.noreply.github.com>
2025-05-10 12:09:03 -0500

    New ground

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { dirname, resolve} from 'node:path'
 import { fileURLToPath} from 'node:url'
 import { defineConfig } from 'vite' 
 
+// Dirname definition
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
+    // Build configuration
     build: {
         rollupOptions: {
             input: {
